51d4b9287ed77cc3250960f4410a0e9b6510c071
lstrauch <lstrauch@salesforce.com>
2021-01-08 19:25:01 +0100

    Anzahl beliebter Titel im jeweililgen genre

--- Trending_Genre.py
+++ Trending_Genre.py
@@ -7,34 +7,34 @@ dataset = pd.read_csv("trending_combined.csv")
 all = []
 genres = []
 years = []
-comedy = {}
-adventure = {}
-animation = {}
-action = {}
-horror = {}
-documentary = {}
-crime = {}
-drama = {}
-romance = {}
-short = {}
-biography = {}
-news = {}
-thriller = {}
-talk_show = {}
-reality_tv = {}
-fantasy = {}
-history = {}
-sci_fi = {}
-mystery = {}
-family = {}
-western = {}
-music = {}
-adult = {}
-musical = {}
-game_show = {}
-sport = {}
-war = {}
-other = {}
+comedy = []
+adventure = []
+animation = []
+action = []
+horror = []
+documentary = []
+crime = []
+drama = []
+romance = []
+short = []
+biography = []
+news = []
+thriller = []
+talk_show = []
+reality_tv = []
+fantasy = []
+history = []
+sci_fi = []
+mystery = []
+family = []
+western = []
+music = []
+adult = []
+musical = []
+game_show = []
+sport = []
+war = []
+other = []
 
 
 for genre in dataset['Genre']:
@@ -47,96 +47,119 @@ for genre in dataset['Genre']:
         else:
             genres.append(genre)
 
+print(genres)
+
 for i in range(2014, 2021):
-    comedy[i] = 0
-    adventure[i] = 0
-    animation[i] = 0
-    action[i] = 0
-    horror[i] = 0
-    documentary[i] = 0
-    crime[i] = 0
-    drama[i] = 0
-    romance[i] = 0
-    short[i] = 0
-    biography[i] = 0
-    news[i] = 0
-    thriller[i] = 0
-    talk_show[i] = 0
-    reality_tv[i] = 0
-    fantasy[i] = 0
-    history[i] = 0
-    sci_fi[i] = 0
-    mystery[i] = 0
-    family[i] = 0
-    western[i] = 0
-    music[i] = 0
-    adult[i] = 0
-    musical[i] = 0
-    game_show[i] = 0
-    sport[i] = 0
-    war[i] = 0
-    other[i] = 0
+    comedy.append(0)
+    comedy.append("Comedy")
+    adventure.append(0)
+    adventure.append("Adventure")
+    animation.append(0)
+    animation.append("Animation")
+    action.append(0)
+    action.append("Action")
+    horror.append(0)
+    horror.append("Horror")
+    documentary.append(0)
+    documentary.append("Documentary")
+    crime.append(0)
+    crime.append("Crime")
+    drama.append(0)
+    drama.append("Drama")
+    romance.append(0)
+    romance.append("Romance")
+    short.append(0)
+    short.append("Short")
+    biography.append(0)
+    biography.append("Biography")
+    news.append(0)
+    news.append("News")
+    thriller.append(0)
+    thriller.append("Thriller")
+    talk_show.append(0)
+    talk_show.append("Talk-Show")
+    reality_tv.append(0)
+    reality_tv.append("Reality-TV")
+    fantasy.append(0)
+    fantasy.append("Fantasy")
+    history.append(0)
+    history.append("History")
+    sci_fi.append(0)
+    sci_fi.append("Sci-Fi")
+    mystery.append(0)
+    mystery.append("Mystery")
+    family.append(0)
+    family.append("Family")
+    western.append(0)
+    western.append("Western")
+    music.append(0)
+    music.append("Music")
+    adult.append(0)
+    adult.append("Adult")
+    musical.append(0)
+    musical.append("Musical")
+    game_show.append(0)
+    game_show.append("Game-Show")
+    sport.append(0)
+    sport.append("Sport")
+    war.append(0)
+    war.append("War")
 
-for year_mixed, genre in zip(dataset['Year'], dataset['Genre']):
-    if year_mixed != '-' and int(year_mixed) >= 2014:
-        year = int(year_mixed)
-        if "Comedy" in genre:
-            comedy[year] += 1
-        elif "Adventure" in genre:
-            adventure[year] += 1
-        elif "Animation" in genre:
-            animation[year] += 1
-        elif "Action" in genre:
-            action[year] += 1
-        elif "Horror" in genre:
-            horror[year] += 1
-        elif "Documentary" in genre:
-            documentary[year] += 1
-        elif "Crime" in genre:
-            crime[year] += 1
-        elif "Drama" in genre:
-            drama[year] += 1
-        elif "Romance" in genre:
-            romance[year] += 1
-        elif "Short" in genre:
-            short[year] += 1
-        elif "Biography" in genre:
-            biography[year] += 1
-        elif "News" in genre:
-            news[year] += 1
-        elif "Thriller" in genre:
-            thriller[year] += 1
-        elif "Talk-Show" in genre:
-            talk_show[year] += 1
-        elif "Reality-TV" in genre:
-            reality_tv[year] += 1
-        elif "Fantasy" in genre:
-            fantasy[year] += 1
-        elif "History" in genre:
-            history[year] += 1
-        elif "Sci-Fi" in genre:
-            sci_fi[year] += 1
-        elif "Mystery" in genre:
-            mystery[year] += 1
-        elif "Family" in genre:
-            family[year] += 1
-        elif "Western" in genre:
-            western[year] += 1
-        elif "Music" in genre:
-            music[year] += 1
-        elif "Adult" in genre:
-            adult[year] += 1
-        elif "Musical" in genre:
-            musical[year] += 1
-        elif "Game-Show" in genre:
-            game_show[year] += 1
-        elif "Sport" in genre:
-            sport[year] += 1
-        elif "War" in genre:
-            war[year] += 1
-
-print(comedy)
-print(action)
+for genre in dataset['Genre']:
+    if "Comedy" in genre:
+        comedy[0] += 1
+    elif "Adventure" in genre:
+        adventure[0] += 1
+    elif "Animation" in genre:
+        animation[0] += 1
+    elif "Action" in genre:
+        action[0] += 1
+    elif "Horror" in genre:
+        horror[0] += 1
+    elif "Documentary" in genre:
+        documentary[0] += 1
+    elif "Crime" in genre:
+        crime[0] += 1
+    elif "Drama" in genre:
+        drama[0] += 1
+    elif "Romance" in genre:
+        romance[0] += 1
+    elif "Short" in genre:
+        short[0] += 1
+    elif "Biography" in genre:
+        biography[0] += 1
+    elif "News" in genre:
+        news[0] += 1
+    elif "Thriller" in genre:
+        thriller[0] += 1
+    elif "Talk-Show" in genre:
+        talk_show[0] += 1
+    elif "Reality-TV" in genre:
+        reality_tv[0] += 1
+    elif "Fantasy" in genre:
+        fantasy[0] += 1
+    elif "History" in genre:
+        history[0] += 1
+    elif "Sci-Fi" in genre:
+        sci_fi[0] += 1
+    elif "Mystery" in genre:
+        mystery[0] += 1
+    elif "Family" in genre:
+        family[0] += 1
+    elif "Western" in genre:
+        western[0] += 1
+    elif "Music" in genre:
+        music[0] += 1
+    elif "Adult" in genre:
+        adult[0] += 1
+    elif "Musical" in genre:
+        musical[0] += 1
+    elif "Game-Show" in genre:
+        game_show[0] += 1
+    elif "Sport" in genre:
+        sport[0] += 1
+    elif "War" in genre:
+        war[0] += 1
 # ======================================================================================================================
 
 all.append(adventure)
@@ -167,28 +190,15 @@ all.append(game_show)
 all.append(sport)
 all.append(war)
 
+
 # ========================================= Plotting ==========================================================
-# -Schaue ob ein Genre mehr als 50 mal in einem Jahr hinzugefügt wurde, ansonsten rechne es zu 'other' dazu
-# -Sortiere das Dictionariy von der kleinstens zu groessten jahreszahl
-# -Plotte die Dictionaries
 
-for dict, genre in zip(all, genres):
-    # if max(dict.values()) < 50:
-    #     for year, value in dict.items():
-    #         other[year] += value
-    #     continue
-    sorted_list = sorted(dict.items())
-    sorted_years, sorted_genre = zip(*sorted_list)
-    plt.plot(sorted_years, sorted_genre, label=genre)
-
-other_list = sorted(other.items())
-other_years, other_genre = zip(*other_list)
-plt.plot(other_years, other_genre, label="Other")
+for list in all:
+    if list[0] > 1:
+        plt.bar(list[1], list[0])
 
 # ======================================================================================================================
 
-plt.title("Anzahl hinzugefügter Titel im jeweiligen Genre", fontsize=10)
-plt.xlabel("Jahr")
-plt.ylabel("Anzahl")
-plt.legend(fontsize=8)
+plt.title("Anzahl beliebter Title im jeweiligen Genre", fontsize=10)
+plt.xticks(fontsize=6)
 plt.show()
